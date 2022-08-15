121b1d78d1c061eab9c416611843e072e7e08507
ariqkharisma <ariqkharisma10@gmail.com>
2022-08-15 21:41:53 +0700

    edit CarSection.jsx

--- binar-car-rental/src/Component/CarSection/CarSection.jsx
+++ binar-car-rental/src/Component/CarSection/CarSection.jsx
@@ -6,25 +6,33 @@ import SearchBar from "../SearchBar/SearchBar";
 
 
 function CarSection() {
-    const [cars, setCars] = useState();
-    const [carName, setCarName] = useState();
+    const [cars, setCars] = useState([]);
+    const [carName, setCarName] = useState('');
+    const [loading, setLoading] = useState(false);
     const [searchParams, setSearchParams] = useSearchParams();
     const carNameParams = searchParams.get('carName');
     const category = searchParams.get('category');
     const price = searchParams.get('price');
 
+    const loadCar = async () => {
+        setLoading(true);
+        try {
+            const { data } = await axios.get('https://bootcamp-rent-car.herokuapp.com/admin/car/');
+            if (carNameParams) {
+                setCars(data.filter((car) => { return car.name.toLowerCase() == carNameParams.toLowerCase() && car.category == category }))
+            } else if (carName) {
+                setCars(data.filter(car => {return car.name.toLowerCase().includes(carName.toLowerCase())}))
+            } else {
+                setCars(data)
+            }
+        } catch (error) {
+          console.log(error);
+        }
+        setLoading(false);
+      };
+
     useEffect(() => {
-            axios.get('https://bootcamp-rent-car.herokuapp.com/admin/car/')
-            .then(res => {
-                if (carNameParams) {
-                    setCars(res.data.filter((car) => { return car.name.toLowerCase() == carNameParams.toLowerCase() && car.category == category }))
-                } else if (carName) {
-                    setCars(res.data.filter(car => {return car.name.toLowerCase().includes(carName.toLowerCase())}))
-                } else {
-                    setCars(res.data)
-                }
-            })
-        
+            loadCar();        
     },[carName])
 
     return (
@@ -33,22 +41,27 @@ function CarSection() {
             <section id="Cars">
                 <div className="container">
                     <div className="row">
-                    {   
-                        cars &&
-                        cars.map((car, index) => {
-                            return (
-                                <div key={index} className="col-lg-4">                   
-                                    <div className="card p-3 d-flex flex-column justify-content-between" style={{height: '100%'}}>
-                                        <img src={car.image} alt="" style={{width: '100%'}} />
-                                        <p>{car.name}</p>
-                                        <h5>Rp {car.price.toLocaleString('en-US')}</h5>
-                                        <p className="fw-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-
-                                        <a href={`/car/${car.id}`} className="btn btn-success" style={{width:"100%"}}>Pilih Mobil</a>
-                                    </div>
-                                </div>
+                    {   !loading ? (
+                            cars? (
+                                cars.map((car, index) => {
+                                    return (
+                                        <div key={index} className="col-lg-4">                   
+                                            <div className="card p-3 d-flex flex-column justify-content-between" style={{height: '100%'}}>
+                                                <img src={car.image} alt="" style={{width: '100%'}} />
+                                                <p>{car.name}</p>
+                                                <h5>Rp {car.price.toLocaleString('en-US')}</h5>
+                                                <p className="fw-bold">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                                                <a href={`/car/${car.id}`} className="btn btn-success" style={{width:"100%"}}>Pilih Mobil</a>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            ) : (
+                                <h1>Mobil Tidak Ditemukan</h1>
                             )
-                        })
+                        ) : (
+                            <h1 className="text-center">Loading....</h1>
+                        )
                     }
                     </div>
                 </div>
