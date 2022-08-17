d7f90cf8e7feef476660790ce762cbf73a66ed19
ariqkharisma <ariqkharisma10@gmail.com>
2022-08-17 11:57:36 +0700

    edit

--- binar-car-rental/src/Component/Faq/Faq.jsx
+++ binar-car-rental/src/Component/Faq/Faq.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useState } from "react";
+import './faq.css'
 
 
 function Faq() {
+    const [faq1, setFaq1] = useState('none');
+    const [faq2, setFaq2] = useState('none');
+    const [faq3, setFaq3] = useState('none');
+    const [faq4, setFaq4] = useState('none');
+    const [faq5, setFaq5] = useState('none');
+    
     return (
         <section id="faq" className="mt-5" style={{minHeight: '400px'}}>
             <div className="container">
@@ -12,21 +20,30 @@ function Faq() {
                     </div>
                     <div className="col-lg-6">
                         <div className="row mx-1">
-                            <div className="card pt-3">
+                            <div onClick={() => faq1 == 'none'? setFaq1('flex') : setFaq1('none')} className="faq-item card pt-3">
                                 <p className=" d-flex justify-content-between">Apa saja syarat yang dibutuhkan? <i className="fa-solid fa-chevron-down"></i></p> 
                             </div>
-                            <div className="card pt-3">
+                            <p style={{display: `${faq1}`}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                            
+                            <div onClick={() => faq2 == 'none'? setFaq2('flex') : setFaq2('none')} className=" faq-item card pt-3">
                                 <p className=" d-flex justify-content-between">Berapa hari minimal sewa mobil lepas kunci? <i className="fa-solid fa-chevron-down "></i></p> 
                             </div>
-                            <div className="card pt-3">
+                            <p style={{display: `${faq2}`}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                            
+                            <div onClick={() => faq3 == 'none'? setFaq3('flex') : setFaq3('none')} className="faq-item card pt-3">
                                 <p className=" d-flex justify-content-between">Berapa hari sebelumnya sabaiknya booking sewa mobil? <i className="fa-solid fa-chevron-down "></i></p>
                             </div>
-                            <div className="card pt-3">
+                            <p style={{display: `${faq3}`}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                            
+                            <div onClick={() => faq4 == 'none'? setFaq4('flex') : setFaq4('none')} className="faq-item card pt-3">
                                 <p className=" d-flex justify-content-between">Apakah Ada biaya antar-jemput?<i className="fa-solid fa-chevron-down "></i></p>
                             </div>
-                            <div className="card pt-3">
+                            <p style={{display: `${faq4}`}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+                            
+                            <div onClick={() => faq5 == 'none'? setFaq5('flex') : setFaq5('none')} className="faq-item card pt-3">
                                 <p className=" d-flex justify-content-between">Bagaimana jika terjadi kecelakaan<i className="fa-solid fa-chevron-down "></i></p> 
                             </div>
+                            <p style={{display: `${faq5}`}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
                         </div>
                     </div>
                 </div>
--- binar-car-rental/src/Component/Testimonial/Testimonial.jsx
+++ binar-car-rental/src/Component/Testimonial/Testimonial.jsx
@@ -10,60 +10,65 @@ function Testimonial() {
                 </div>
 
                 <div className="d-flex">
-                <div className="card d-flex p-3 border" style={{width: '619px', minHeight: '270px', backgroundColor: '#F1F3FF'}}>
-                    <div className="row border">
-                        <div className="col-lg-3 d-flex align-items-center">
-                            <img src="./Assets/img_photo.png" alt="person" />
+                    <div className="card d-flex p-3 border" style={{width: '619px', minHeight: '270px', backgroundColor: '#F1F3FF'}}>
+                        <div className="row border">
+                            <div className="col-lg-3 d-flex align-items-center">
+                                <img src="./Assets/img_photo.png" alt="person" />
+                            </div>
+                            <div className="col-lg-9">
+                                <div className="star my-3">
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                </div>
+                                <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”</p>
+                                <p>John Dee 32, Bromo</p>
+                            </div>
                         </div>
-                        <div className="col-lg-9">
+                    </div>
+                    <div className="card p-3 d-flex border" style={{width: '619px', minHeight: '270px', backgroundColor: '#F1F3FF'}}>
+                        <div className="row border">
+                            <div className="col-lg-3 d-flex align-items-center">
+                                <img src="./Assets/img_photo.png" alt="person" />
+                            </div>
+                            <div className="col-lg-9">
                             <div className="star my-3">
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                </div>
+                                <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”</p>
+                                <p>John Dee 32, Bromo</p>
                             </div>
-                            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”</p>
-                            <p>John Dee 32, Bromo</p>
+                        </div>
+                    </div>
+                    <div className="card p-3 d-flex" style={{width: '619px', minHeight: '270px', backgroundColor: '#F1F3FF'}}>
+                        <div className="row border">
+                            <div className="col-lg-3 d-flex align-items-center">
+                                <img src="./Assets/img_photo.png" alt="person" />
+                            </div>
+                            <div className="col-lg-9">
+                            <div className="star my-3">
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                    <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
+                                </div>
+                                <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”</p>
+                                <p>John Dee 32, Bromo</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="card p-3 d-flex border" style={{width: '619px', minHeight: '270px', backgroundColor: '#F1F3FF'}}>
-                    <div className="row border">
-                        <div className="col-lg-3 d-flex align-items-center">
-                            <img src="./Assets/img_photo.png" alt="person" />
-                        </div>
-                        <div className="col-lg-9">
-                        <div className="star my-3">
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                            </div>
-                            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”</p>
-                            <p>John Dee 32, Bromo</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="card p-3 d-flex" style={{width: '619px', minHeight: '270px', backgroundColor: '#F1F3FF'}}>
-                    <div className="row border">
-                        <div className="col-lg-3 d-flex align-items-center">
-                            <img src="./Assets/img_photo.png" alt="person" />
-                        </div>
-                        <div className="col-lg-9">
-                        <div className="star my-3">
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                                <i className="fa-solid fa-star" style={{color: '#F9CC00'}}></i>
-                            </div>
-                            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod”</p>
-                            <p>John Dee 32, Bromo</p>
-                        </div>
-                    </div>
-                </div>
+
+                <div className="control container d-flex justify-content-center">
+                    <button className="btn btn-light my-3 mr-3" style={{border: 'gray solid 1px', borderRadius: '50%'}}>&#60;</button>
+                    <button className="btn my-3" style={{backgroundColor: '#5CB85F', borderRadius: '50%', color: 'white'}}>&#62;</button>
                 </div>
         </section>
     )
