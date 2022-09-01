5b65e16f689d4df0cda40fc30ed3d8b275ae1491
ariqkharisma <ariqkharisma10@gmail.com>
2022-09-02 06:48:19 +0700

    edit

--- binar-car-rental-customer/src/Component/SignupSection/SignupSection.jsx
+++ binar-car-rental-customer/src/Component/SignupSection/SignupSection.jsx
@@ -1,28 +1,86 @@
 import React from "react";
-import "./SignupSection.css"
+import "./SignupSection.css";
+import * as Yup from 'yup'
+import { useFormik } from "formik";
 
 
 
 function SignupSection() {
+    const signupSchema = Yup.object().shape({
+        name: Yup.string()
+        .required("Name is required")
+        .min(3, "Name must be 3 characters at minimum")
+        .max(15, "Name must be 15 characters at maximum"),
+        email: Yup.string()
+        .email("Invalid email adress format")
+        .required("Password is required"),
+        password: Yup.string()
+        .required("Password is required")
+        .min(6, "Password must be 6 characters at minimum"),
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+        },
+        validationSchema: signupSchema,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    })
+    
     return (
         <section id="signupSection" className="mb-0">
             <div className="container-fluid">
                 <div className="row min-vh-100">
                     <div className="leftside col-lg-6 d-flex flex-column justify-content-sm-center align-items-center">
-                    <form className="mx-3">
+                    <form className="mx-3" onSubmit={formik.handleSubmit}>
                         <div className="rectangle"></div>
                         <h1 className="w-100 my-4">Sign Up</h1>
                         <div className="form-group">
                             <label htmlFor="name">Name*</label>
-                            <input type="name" className="form-control" id="name" aria-describedby="name" placeholder="Full Name"/>
+                            <input  
+                                className="form-control" 
+                                aria-describedby="name" 
+                                placeholder="Full Name"
+                                id="name"
+                                name="name"
+                                type="text"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.name} 
+                            />
+                            {formik.touched.name && formik.errors.name ? <div className="text-danger mt-1">{formik.errors.name}</div> : null}
                         </div>
                         <div className="form-group">
                             <label htmlFor="email">Email*</label>
-                            <input type="email" className="form-control" id="email" placeholder="Contoh: johndoe@gmail.com"/>
+                            <input 
+                                className="form-control" 
+                                placeholder="Contoh: johndoe@gmail.com"
+                                id="email" 
+                                name="email"
+                                type="email" 
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email}
+                            />
+                            {formik.touched.email && formik.errors.email ? <div className="text-danger mt-1">{formik.errors.email}</div> : null}
                         </div>
                         <div className="form-group">
-                            <label htmlFor="createPassword">Create Password*</label>
-                            <input type="password" className="form-control" id="createPassword" placeholder="6+ Karakter"/>
+                            <label htmlFor="password">Create Password*</label>
+                            <input 
+                                placeholder="6+ Karakter"
+                                className="form-control" 
+                                id="password" 
+                                name="password"
+                                type="password"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.password}
+                            />
+                            {formik.touched.password && formik.errors.password ? <div className="text-danger mt-1">{formik.errors.password}</div> : null}
                         </div>
                         <button type="submit" className="btn mt-3">Sign Up</button>
                         <p className="mt-4 d-flex justify-content-center">Already have an account?<a href="/login">Sign In here</a></p>
