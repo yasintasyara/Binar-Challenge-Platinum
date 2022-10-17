53da33a2381b47198cba8e7050c0acd51fcbf645
Shania Shamanta <shaniashamantha@gmail.com>
2022-10-17 10:24:25 +0700

    Add Middleware for Detail Order

--- binar-car-rental-customer/src/Component/DetailPayment/DetailPaymentSection.jsx
+++ binar-car-rental-customer/src/Component/DetailPayment/DetailPaymentSection.jsx
@@ -1,13 +1,21 @@
-import { current } from "@reduxjs/toolkit";
 import React, { Fragment } from "react";
 import { useState } from "react";
 import SearchBarPayment from "../SearchBar/SearchBarPayment";
 import "./detailPayment.css";
+import axios from "axios";
+import { useEffect } from "react";
 
+const API_URL = "https://bootcamp-rent-car.herokuapp.com/customer/order";
+const getLocalStorage = JSON.parse(localStorage.getItem("user"));
 function DetailPaymentSection() {
   const [isActive, setIsActive] = useState(false);
   const [isActive2, setIsActive2] = useState(false);
   const [isActive3, setIsActive3] = useState(false);
+  const [detailCar, setDetailCar] = useState({});
+  const [detailOrder, setDetailOrder] = useState({});
+
+  const [loading, setLoading] = useState(false);
+
 
   const handleClick = () => {
     setIsActive((current) => !current);
@@ -33,129 +41,184 @@ function DetailPaymentSection() {
       : true;
   };
 
-  return (
-    <>
-      <SearchBarPayment />
-      <section className="mb-5" id="detailPayment">
-        <div className="container">
-          <div className="row justify-content-between">
-            <div className="col-12 col-md-6 card py-4 payment-card h-auto">
-              <h5>Pilih Bank Transfer</h5>
-              <p>
-                Kamu bisa membayar dengan transfer melalui ATM, Internet Banking
-                atau Mobile Banking
-              </p>
-              <ul
-                className="list-group list-group-flush"
-                style={{ border: "none" }}
-              >
-                <li className="list-group-item flex">
-                  <button
-                    onClick={handleClick}
-                    className={isActive ? "button-bank active" : "button-bank"}
-                  >
-                    <div className="d-flex align-items-center">
-                      <div className="bank-text">BCA</div>
-                      <div className="bank-text-info">BCA Transfer</div>
-                    </div>
-                  </button>
-                </li>
-                <li className="list-group-item ">
-                  <button
-                    onClick={handleClick2}
-                    className={isActive2 ? "button-bank active" : "button-bank"}
-                  >
-                    <div className="d-flex align-items-center">
-                      <div className="bank-text">BNI</div>
-                      <div className="bank-text-info">BNI Transfer</div>
-                    </div>
-                  </button>
-                </li>
-                <li className="list-group-item">
-                  <button
-                    onClick={handleClick3}
-                    className={isActive3 ? "button-bank active" : "button-bank"}
-                  >
-                    <div className="d-flex align-items-center">
-                      <div className="bank-text">Mandiri</div>
-                      <div className="bank-text-info">Mandiri Transfer</div>
-                    </div>
-                  </button>
-                </li>
-              </ul>
-            </div>
+  const config = {
+    headers: { access_token: getLocalStorage.access_token },
+  };
 
-            <div className="col-12 col-md-5 card payment-card">
-              <div className="detail-payment">
-                <div className="detail-payment-item">
-                  <h4>Innova</h4>
-                </div>
-                <div className="detail-payment-description">
-                  <div className="header-detail d-flex justify-content-between">
-                    <h5>Total</h5>
-                    <h5>Rp. 3.500.000</h5>
-                  </div>
-                </div>
-                <div className="content-detail mt-3">
-                  <div className="content-detail-section">
-                    <h5>Harga</h5>
+  const getDetailCar = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.get(`${API_URL}/328`, config);
+      setDetailOrder(res.data);
+      setDetailCar(res.data.Car);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-                    <ul className="lh-lg" style={{ listStyleType: "disc" }}>
-                      <li className="d-flex justify-content-between mb-1">
-                        <p className="">Sewa Mobil Rp.500.000 X 7 Hari</p>
-                        <p className="">Rp. 3.500.000</p>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="content-detail-section">
-                    <h5>Biaya Lainnya</h5>
-                    <ul className="lh-lg" style={{ listStyleType: "disc" }}>
-                      <li className="d-flex justify-content-between mb-1">
-                        <p className="">Pajak</p>
-                        <p className="text-green">Termasuk</p>
-                      </li>
-                      <li className="d-flex justify-content-between mb-1">
-                        <p className="">Biaya Makan Supir</p>
-                        <p className="text-green">Termasuk</p>
-                      </li>
-                    </ul>
-                  </div>
-                  <div className="content-detail-section">
-                    <h5>Belum Termasuk</h5>
-                    <ul className="lh-lg" style={{ listStyleType: "disc" }}>
-                      <li className="d-flex justify-content-between mb-1">
-                        <p className="">Bensin</p>
-                      </li>
-                      <li className="d-flex justify-content-between mb-1">
-                        <p className="">Tol dan Parkir</p>
-                      </li>
-                    </ul>
-                  </div>
-                  <hr />
-                  <div className="total-payment">
-                    <div className="d-flex justify-content-between my-3">
-                      <h5>Total</h5>
-                      <h5>Rp. 3.500.000</h5>
-                    </div>
+  useEffect(() => {
+    getDetailCar();
+  }, []);
+
+ 
+  const findDayDifference = (startDate, endDate) => {
+    const DAY_UNIT_IN_MILLISECONDS = 24 * 3600 * 1000;
+    const diffMiliSeconds =
+      new Date(endDate).getTime() - new Date(startDate).getTime();
+    return diffMiliSeconds / DAY_UNIT_IN_MILLISECONDS;
+  };
+
+  const formatterRupiah = (price) => {
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(price);
+  };
+
+  if (loading) {
+    return
+  } else {
+    return (
+      <>
+        <SearchBarPayment detailCar={detailOrder} />
+        <section className="mb-5" id="detailPayment">
+          <div className="container">
+            <div className="row justify-content-between">
+              <div className="col-12 col-md-6 card py-4 payment-card h-auto">
+                <h5>Pilih Bank Transfer</h5>
+                <p>
+                  Kamu bisa membayar dengan transfer melalui ATM, Internet
+                  Banking atau Mobile Banking
+                </p>
+                <ul
+                  className="list-group list-group-flush"
+                  style={{ border: "none" }}
+                >
+                  <li className="list-group-item flex">
                     <button
-                      type="button"
-                      disabled={checkButtonPayment()}
+                      onClick={handleClick}
                       className={
-                        "cst-button" +
-                        (checkButtonPayment() == true ? " disabled" : "")
+                        isActive ? "button-bank active" : "button-bank"
                       }
                     >
-                      Bayar
+                      <div className="d-flex align-items-center">
+                        <div className="bank-text">BCA</div>
+                        <div className="bank-text-info">BCA Transfer</div>
+                      </div>
                     </button>
+                  </li>
+                  <li className="list-group-item ">
+                    <button
+                      onClick={handleClick2}
+                      className={
+                        isActive2 ? "button-bank active" : "button-bank"
+                      }
+                    >
+                      <div className="d-flex align-items-center">
+                        <div className="bank-text">BNI</div>
+                        <div className="bank-text-info">BNI Transfer</div>
+                      </div>
+                    </button>
+                  </li>
+                  <li className="list-group-item">
+                    <button
+                      onClick={handleClick3}
+                      className={
+                        isActive3 ? "button-bank active" : "button-bank"
+                      }
+                    >
+                      <div className="d-flex align-items-center">
+                        <div className="bank-text">Mandiri</div>
+                        <div className="bank-text-info">Mandiri Transfer</div>
+                      </div>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-12 col-md-5 card payment-card">
+                <div className="detail-payment">
+                  <div className="detail-payment-item">
+                    <h4>{detailCar.name}</h4>
+                  </div>
+                  <div className="detail-payment-description">
+                    <div className="header-detail d-flex justify-content-between">
+                      <h5>Total</h5>
+                      <h5>{formatterRupiah(detailOrder.total_price)}</h5>
+                    </div>
+                  </div>
+                  <div className="content-detail mt-3">
+                    <div className="content-detail-section">
+                      <h5>Harga</h5>
+
+                      <ul className="lh-lg" style={{ listStyleType: "disc" }}>
+                        <li className="d-flex justify-content-between mb-1">
+                          <p className="">
+                            Sewa Mobil {formatterRupiah(detailCar.price)} X{" "}
+                            {findDayDifference(
+                              detailOrder.start_rent_at,
+                              detailOrder.finish_rent_at
+                            )}{" "}
+                            Hari
+                          </p>
+                          <p className="">
+                            {formatterRupiah(detailOrder.total_price)}
+                          </p>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="content-detail-section">
+                      <h5>Biaya Lainnya</h5>
+                      <ul className="lh-lg" style={{ listStyleType: "disc" }}>
+                        <li className="d-flex justify-content-between mb-1">
+                          <p className="">Pajak</p>
+                          <p className="text-green">Termasuk</p>
+                        </li>
+                        <li className="d-flex justify-content-between mb-1">
+                          <p className="">Biaya Makan Supir</p>
+                          <p className="text-green">Termasuk</p>
+                        </li>
+                      </ul>
+                    </div>
+                    <div className="content-detail-section">
+                      <h5>Belum Termasuk</h5>
+                      <ul className="lh-lg" style={{ listStyleType: "disc" }}>
+                        <li className="d-flex justify-content-between mb-1">
+                          <p className="">Bensin</p>
+                        </li>
+                        <li className="d-flex justify-content-between mb-1">
+                          <p className="">Tol dan Parkir</p>
+                        </li>
+                      </ul>
+                    </div>
+                    <hr />
+                    <div className="total-payment">
+                      <div className="d-flex justify-content-between my-3">
+                        <h5>Total</h5>
+                        <h5>{formatterRupiah(detailOrder.total_price)}</h5>
+                      </div>
+                      <button
+                        type="button"
+                        disabled={checkButtonPayment()}
+                        className={
+                          "cst-button" +
+                          (checkButtonPayment() == true ? " disabled" : "")
+                        }
+                      >
+                        Bayar
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
-    </>
-  );
+        </section>
+      </>
+    );
+  }
 }
 
 export default DetailPaymentSection;
--- binar-car-rental-customer/src/Component/SearchBar/SearchBarPayment.jsx
+++ binar-car-rental-customer/src/Component/SearchBar/SearchBarPayment.jsx
@@ -1,40 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 import { createSearchParams, useNavigate } from "react-router-dom";
 import "./style.css";
 
-const useNavigateSearch = () => {
-  const navigate = useNavigate();
-  return (pathName, params) =>
-    navigate(`${pathName}?${createSearchParams(params)}`);
-};
+function SearchBarPayment({ detailCar }) {
 
-function SearchBarPayment({
-  carName,
-  setCarName,
-  carCategory,
-  setCarCategory,
-  carPrice,
-  setCarPrice,
-  carStatus,
-  setCarStatus,
-  setSearchParams,
-}) {
-  const navigateSearch = useNavigateSearch();
-  const goToSearch = () =>
-    navigateSearch("/search", {
-      name: `${carName}`,
-      category: `${carCategory}`,
-      price: `${carPrice}`,
-      status: `${carStatus}`,
-    });
+  const [carDetail, setCarDetail] = useState({})
+  const [detailOrder, setDetailOrder] = useState({})
 
-  const handleOnClick = (e) => {
-    e.preventDefault();
-    if (carName && carCategory && carPrice && carStatus) {
-      goToSearch();
-    } else alert("Semua Kolom Pencarian Harus Diisi");
+  
+  useEffect(() => {
+    if (detailCar) {
+      setCarDetail(detailCar.Car);
+      setDetailOrder(detailCar);
+
+    }
+  }, [detailCar]);
+
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString();
   };
 
+  if(!detailCar) return
   return (
     <section
       id="searchBarPayment"
@@ -69,37 +57,33 @@ function SearchBarPayment({
           style={{ boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)", width: "100%" }}
         >
           <h4>Detail Pesananmu</h4>
-            <div className="row mt-3">
-              <div className="col-lg-3 d-flex flex-column justify-content-center">
-                <label htmlFor="name">Nama/Tipe Mobil</label>
-                <div className="text-secondary">
-                    Innova
-                </div>
-              </div>
-
-              <div className="col-lg-3 d-flex flex-column justify-content-center">
-                <label htmlFor="category">Kategori</label>
-                <div className="text-secondary">
-                    6 - 8 orang
-                </div>
-              </div>
-
-              <div className="col-lg-3 d-flex flex-column justify-content-center">
-                <label htmlFor="price">Tanggal Mulai Sewa</label>
-                <div className="text-secondary">
-                    2 Juni 2022
-                </div>
-              </div>
-
-              <div className="col-lg-2 d-flex flex-column justify-content-center">
-                <label htmlFor="status">Tanggal Akhir Sewa</label>
-                <div className="text-secondary">    
-                    8 Juni 2022
-                </div>
-              </div>
-
-              <div className="col-lg-1 d-flex align-items-end"></div>
+          <div className="row mt-3">
+            <div className="col-lg-3 d-flex flex-column justify-content-center">
+              <label htmlFor="name">Nama/Tipe Mobil</label>
+              <div className="text-secondary">{carDetail.name}</div>
             </div>
+
+            <div className="col-lg-3 d-flex flex-column justify-content-center">
+              <label htmlFor="category">Kategori</label>
+              <div className="text-secondary">{carDetail.category}</div>
+            </div>
+
+            <div className="col-lg-3 d-flex flex-column justify-content-center">
+              <label htmlFor="price">Tanggal Mulai Sewa</label>
+              <div className="text-secondary">
+                {formatDate(detailOrder.start_rent_at)}
+              </div>
+            </div>
+
+            <div className="col-lg-2 d-flex flex-column justify-content-center">
+              <label htmlFor="status">Tanggal Akhir Sewa</label>
+              <div className="text-secondary">
+                {formatDate(detailOrder.finish_rent_at)}
+              </div>
+            </div>
+
+            <div className="col-lg-1 d-flex align-items-end"></div>
+          </div>
         </div>
       </div>
     </section>
